1187eb93350edf9a78cd106d368b8a4d6f658a48
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-27 16:34:33 +0600

    feat: add ThemeProvider component for theme management

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
+import { ThemeProvider as NextThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <NextThemeProvider>{children}</NextThemeProvider>
       </body>
     </html>
   );
