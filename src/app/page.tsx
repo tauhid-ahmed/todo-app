5e1328e9c93e47e3e2a5cb8bdbc0a5fb48806aa7
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-29 15:43:16 +0600

    feat: demo implement todo fetching and display functionality in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,39 @@
+"use client";
+
+import { useTodo } from "@/hooks/use-todo";
+import { Todo } from "@/types/todo.types";
+import { useEffect } from "react";
+
+const defaultTodos: Todo[] = [
+  {
+    id: "1",
+    title: "Todo 1",
+    description: "Todo Description",
+    status: "pending",
+    category: "Fitness",
+    created_at: new Date(),
+  },
+];
+
 export default function Home() {
-  return <div>Home</div>;
+  const { state, fetchTodos, createTodo } = useTodo();
+
+  useEffect(() => {
+    fetchTodos(defaultTodos);
+  }, []);
+
+  if (state.status === "fetching") {
+    return <div>Loading todos...</div>;
+  }
+
+  return (
+    <div>
+      <button onClick={() => createTodo()}>add todo</button>
+      {state.todos.map((todo) => (
+        <div key={todo.id}>
+          {todo.title} - {todo.description} - status: {todo.status}
+        </div>
+      ))}
+    </div>
+  );
 }
